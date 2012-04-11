63228c99512c371f8cd57ce18fcc1070b59658c4
Rodney Degracia <rdegraci@elvenlabs.com>
2012-04-11 13:58:24 -0400

    Initial Commit

---
+++ ga-demo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ga-demo
+//
+//  Created by Rodney Degracia on 4/11/12.
+//  Copyright (c) 2012 Venture Intellectual LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate, UITabBarControllerDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) UITabBarController *tabBarController;
+
+@end
---
+++ ga-demo/SecondViewController.h
@@ -0,0 +1,13 @@
+//
+//  SecondViewController.h
+//  ga-demo
+//
+//  Created by Rodney Degracia on 4/11/12.
+//  Copyright (c) 2012 Venture Intellectual LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SecondViewController : UIViewController
+
+@end
